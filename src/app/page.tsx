b9687413ea59b2fd4f9251bb0f24730b32ca15d0
Raihan Uddin <raihanuddin2002@gmail.com>
2023-11-23 03:49:13 +0600

    Change brand name 'bachelor' to 'House Rent'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <>
       <section className="h-[calc(100vh-70px)]">
         <div className="h-full flex justify-center items-center">
-          <h1 className="text-4xl font-bold">Welcome to Bachelor BD </h1>
+          <h1 className="text-4xl font-bold">Welcome to House Rent </h1>
         </div>
       </section>
     </>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser } from '@fortawesome/free-solid-svg-icons';
 
 type LogoProps = {
-    width: number;
-    height: number;
-    className: string;
+    className?: string;
+    size?: '2xs' | 'xs' | 'sm' | 'lg' | 'xl' | '2xl'
 }
 
-export default function LogoImage({ width, height, className }: LogoProps) {
+export default function Logo({ className = "", size = 'lg' }: LogoProps) {
     return (
-        <Image src='/images/favicon.ico' className={className} width={width} height={height} alt='Logo' />
+        <FontAwesomeIcon className={className} icon={faHouseUser} size={size} />
     )
 }
